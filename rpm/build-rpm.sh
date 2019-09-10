a9c2d3a539d3f27282ed286fa5226f74e2ad7a73
Thomas Bachman <tbachman@yahoo.com>
2019-09-10 12:23:30 +0000

    Fix RPM build

--- rpm/build-rpm.sh
+++ rpm/build-rpm.sh
@@ -5,7 +5,7 @@ BUILD_DIR=${BUILD_DIR:-`pwd`/rpmbuild}
 mkdir -p $BUILD_DIR/BUILD $BUILD_DIR/SOURCES $BUILD_DIR/SPECS $BUILD_DIR/RPMS $BUILD_DIR/SRPMS
 RELEASE=${RELEASE:-1}
 VERSION=`python setup.py --version`
-SPEC_FILE=aci-vpplib.spec
+SPEC_FILE=nova-vpp-vifs.spec
 sed -e "s/@VERSION@/$VERSION/" -e "s/@RELEASE@/$RELEASE/" rpm/$SPEC_FILE.in > $BUILD_DIR/SPECS/$SPEC_FILE
 python setup.py sdist --dist-dir $BUILD_DIR/SOURCES
 rpmbuild --clean -ba --define "_topdir $BUILD_DIR" $BUILD_DIR/SPECS/$SPEC_FILE
